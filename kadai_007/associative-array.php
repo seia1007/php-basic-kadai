cbf93e54e7b65cf50702e09d6479aa9568d3f22d
seia1007 <1007seia@gmail.com>
2024-12-28 00:37:53 +0900

    kadai_007アップロード

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>kadai_007</title>
+</head>
+<body>
+  <p>
+    <?php
+    $associative=['name'=>'onion', 'price'=>200, 'weight'=>160];
+    print_r($associative);
+    ?>
+  </p>
+</body>
+</html>
